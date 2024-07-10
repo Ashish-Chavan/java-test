4d07b75e278db998722827ab894003d1011a471e
Ashish-Chavan <110707870+Ashish-Chavan@users.noreply.github.com>
2024-07-10 19:31:22 +0530

    Add files via upload

---
+++ SystemSpecsDto.java
@@ -0,0 +1,13 @@
+package com.blueoptima.cs.dao.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SystemSpecsDto {
+
+    public Integer cpu;
+    public String memory;
+    public String disk_available;
+    public String disk_used;
+    public String os;
+}
