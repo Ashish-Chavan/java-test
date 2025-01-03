536d311f3347de456d110e8170b3fa2b939f8fd2
Ash <110707870+Ashish-Chavan@users.noreply.github.com>
2025-01-03 18:29:08 +0530

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,10 @@
+def complex_function(a, b, c):
+    if a > 0 and b < 10:
+        if c == 5:
+            return "A"
+        elif c > 5:
+            return "B"
+        else:
+            return "C"
+    else:
+        return "D"
